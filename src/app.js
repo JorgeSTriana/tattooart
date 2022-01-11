f8cbd50e6434efb0eb02ac1e927b81c3a6dda4c2
JorgeSTriana <jorgetriana1951@gmail.com>
2022-01-11 16:36:16 -0500

    heroku first push

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require ('express')
 const morgan = require('morgan')
-const mongoose = require('moongose')
+const mongoose = require('mongoose')
 const cors = require('cors')
 require('dotenv').config()
 const app = express()
