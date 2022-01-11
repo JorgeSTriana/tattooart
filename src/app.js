93be1359fdc80ccf6436345b0d8cdaff7a517b5b
JorgeSTriana <jorgetriana1951@gmail.com>
2022-01-11 18:24:52 -0500

    heroku first push

--- src/app.js
+++ src/app.js
@@ -2,17 +2,16 @@ const express = require ('express')
 const morgan = require('morgan')
 const mongoose = require('mongoose')
 const cors = require('cors')
+const path = require('path')
 require('dotenv').config()
 const app = express()
 const authRoutes = require('./routes/auth.routes')
-const { db } = require('./models/user')
+
 
 
 /* configuraciones */
 app.set('port', process.env.PORT || 3000)
-mongoose.connect(process.env.DB_STRING)
-.then(db => console.log('Connected to Mongo'))
-.catch(err => console.log(err))
+mongoose.connect(process.env.DB_STRING).then(db => console.log('Connected to Mongo')).catch(err => console.log(err))
 
 /* middlewares */
 app.use(morgan('dev'))
