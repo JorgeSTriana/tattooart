aeae3014c76ec1879b3563b6f686268d7103f9b9
JorgeSTriana <jorgetriana1951@gmail.com>
2022-01-19 14:46:53 -0500

    conexion a db

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -4,6 +4,18 @@ const msg = require('../helpers/messages')
 const User = require('../models/user')
 const authService = require('../services/auth.service')
 
+router.get('/profile', async (req, res)=>{
+    try {
+        const user = new User(req.body)
+        // let token = await authService.register(user)
+        // res.status(200).json({"token": token})
+        res.send("bien")
+    } catch (error) {
+        res.send(error)
+        // res.status(500).json({'error':error})
+    }
+})
+
 router.post('/register', async (req, res)=>{
     try {
         const user = new User(req.body)
